4bd6934af34da5d24260f4a121cf7a08e4cc3df0
Christina <christina@Christinas-Mac-Air.local>
2015-09-01 17:50:14 -0400

    Refactoring routes to use resources

--- photogur/config/routes.rb
+++ photogur/config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
   delete 'pictures/:id' => 'pictures#destroy', as: 'delete_picture'
 
   root 'pictures#index'
+
+    resources :pictures
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
